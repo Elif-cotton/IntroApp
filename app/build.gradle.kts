e852a9938ea14959f129839bf9aab6037881db87
“elif-cotton” <elif.watercolors@gmail.com>
2024-09-15 19:13:08 -0300

    Agregar dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.accompanist.pager)
+    implementation(libs.lottie.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(kotlin("script-runtime"))
+
 }
